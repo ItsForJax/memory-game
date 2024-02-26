5abb31da5e676b9919d1eff3d5b0031cfcbda480
4jax <dadelos.ddls@gmail.com>
2024-02-26 20:26:27 +0800

    Added submit score to firebase database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import CardComponent from './components/CardComponent';
+import { initializeApp } from "firebase/app";
+// import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+import { uid } from "uid";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkAvj1n4yzqjkOeEP_3kXTDFeS8ivZ90U",
+  authDomain: "memory-game-leader-boards.firebaseapp.com",
+  databaseURL: "https://memory-game-leader-boards-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "memory-game-leader-boards",
+  storageBucket: "memory-game-leader-boards.appspot.com",
+  messagingSenderId: "217594973842",
+  appId: "1:217594973842:web:26aecc74e7a95de07b604b",
+  measurementId: "G-K4MH92YXLV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+// const auth = getAuth(app);
+
+// const authProvider = new GoogleAuthProvider()
 
 // Cards constants
 const cardImages = [
@@ -18,6 +40,8 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
   const [disabled, setDisabled] = useState(false)
+  // const [loggedin, setLoggedin] = useState(false)
+  const [gameover, setGameover] = useState(false)
 
   // Shuffle card function
   const shuffleCards = () => {
@@ -56,6 +80,10 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
 
+  useEffect(() => {
+    setGameover(cards.every(item => item.matched))
+  },[cards])
+
   // reset choices
   const resetTurn = () => {
     setDisabled(false)
@@ -69,10 +97,55 @@ function App() {
     shuffleCards()
   }, [])
 
+
+  // auth
+  // const signin = () => {
+  //   signInWithPopup(auth, authProvider)
+  //   .then((result)=> {
+  //     setLoggedin(true)
+  //   })
+  //   .catch((error)=>{
+  //     console.log(error)
+  //   })
+  // }
+
+  // submit to leaderboard
+  const handleSubmitScore = () => {
+    const name = prompt('Enter Name: ')
+    submitScoreToFirebare(name)
+    setGameover(false)
+    shuffleCards()
+  }
+
+  //submit score to the leaderboard
+  const submitScoreToFirebare = (name) => {
+    const uuid = uid()
+    set(ref(db, `/${uuid}`), {
+      name: name,
+      score: turns
+    })
+  }
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
+      {/* {
+        loggedin ? 
+        <button onClick={() => {auth.signOut(); setLoggedin(false)}}>Signout</button>
+          :
+        <button onClick={signin}>Signin</button>
+      } */}
+      {
+        gameover ? 
+        <>
+          <h2>Game Over</h2>
+          <button onClick={handleSubmitScore}>Submit Score</button>
+        </>
+       : null
+      }
+      <> </>
       <button onClick={shuffleCards}>New Game</button>
+      
       <div className='card-grid'>
         {cards.map(card => (
           <CardComponent 
